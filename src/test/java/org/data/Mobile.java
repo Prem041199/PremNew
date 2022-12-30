8090bed64fa1aca7b6c43bbe144d469c707a92ad
Prem041199 <premkumarmettur@gmail.com>
2022-12-30 11:08:43 +0530

    New code is commit

---
+++ src/test/java/org/data/Mobile.java
@@ -0,0 +1,17 @@
+package org.data;
+
+public class Mobile {
+	private void call() {
+		System.out.println("Voice call");
+
+	}
+	private void watsap() {
+		System.out.println("Chat");
+
+	}
+	private void videocall() {
+		System.out.println("Videocall with virtual");
+
+	}
+
+}
